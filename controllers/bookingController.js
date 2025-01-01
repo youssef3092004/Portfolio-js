41dcace4138ed4bfdc05f30595b61637082a823f
youssef <youssefahmedabdelkader25@gmail.com>
2025-01-01 15:32:23 +0200

    Add controller function to create a booking
    - Implement `createBooking` to create a new booking with check-in, check-out, total price, status, user, hotel, room, and discount.
    - Validate that all required fields are provided before saving the booking.
    - Return the saved booking as a JSON response with a 201 status code.
    - Handle errors and return appropriate messages if any required fields are missing or other issues occur.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -33,3 +33,50 @@ const getBooking = async (req, res, next) => {
     next(error);
   }
 };
+
+createBooking = async (req, res, next) => {
+  try {
+    const {
+      check_in,
+      check_out,
+      total_price,
+      status,
+      user,
+      hotel,
+      room,
+      discount,
+    } = req.body;
+    const requiredFields = {
+      check_in,
+      check_out,
+      total_price,
+      status,
+      user,
+      hotel,
+      room,
+      discount,
+    };
+    for (let i in requiredFields) {
+      if (!requiredFields[i]) {
+        res.status(400);
+        throw new Error(
+          `${i.charAt(0).toUpperCase() + i.slice(1)} Is Required`
+        );
+      }
+    }
+    const newBooking = new Booking({
+      check_in,
+      check_out,
+      total_price,
+      status,
+      user,
+      hotel,
+      room,
+      discount,
+    });
+    const savedBooking = await newBooking.save();
+    res.status(201).json(savedBooking);
+  } catch (error) {
+    next(error);
+  }
+};
