1ea63b4162aeb2291fa8bcf3e829fe26ece6ac74
youssef <youssefahmedabdelkader25@gmail.com>
2025-01-01 15:52:37 +0200

    Add controller function to get all amenities
    - Implement `getAmenities` to retrieve all amenities from the database.
    - Return a 404 error if no amenities are found.
    - Populate hotel amenities related data while retrieving amenities.
    - Added detailed JSDoc comment to the `getAmenities` function.
    - Described route, access, expected responses, and error handling.
    - Explained the use of `populate` for fetching related hotel amenities.

---
+++ controllers/amenityController.js
@@ -0,0 +1,25 @@
+const Amenity = require("../models/amenity");
+
+/**
+ * @route GET /api/amenities
+ * @desc Fetch all amenities from the database and return them in the response.
+ * @access Public
+ * @throws {Error} If no amenities are found, returns a 404 status with a relevant error message.
+ * @returns {Object} A JSON array containing all the amenities.
+ *
+ * This route handler fetches all amenities stored in the database. It uses the Mongoose model `Amenity` to query the database,
+ * and the `populate` method is used to retrieve the associated `hotel_amenities`. If no amenities are found, an error is thrown.
+ * The result is returned with a status code of 200. If an error occurs during the operation, it is passed to the error handling middleware.
+ */
+const getAmenities = async (req, res, next) => {
+  try {
+    const amenities = await Amenity.find().populate("hotel_amenities");
+    if (!amenities) {
+      res.status(404);
+      throw new Error("There are no amenities available");
+    }
+    return res.status(200).json(amenities);
+  } catch (error) {
+    next(error);
+  }
+};
