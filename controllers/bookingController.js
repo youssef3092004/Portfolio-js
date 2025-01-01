373c9ba388b7cc3347e0225a676d3832c4b199eb
youssef <youssefahmedabdelkader25@gmail.com>
2025-01-01 14:56:51 +0200

    Add controller function to get booking by ID
    - Implement `getBooking` to retrieve a single booking by ID, with populated fields for user, hotel, room, and discount.
    - Return the booking as a JSON response with a 200 status code.
    - Handle the case where no booking is found by returning a 404 error.
    - Use error handling to pass any errors to the next middleware.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -16,3 +16,20 @@ const getBookings = async (req, res, next) => {
     next(error);
   }
 };
+
+const getBooking = async (req, res, next) => {
+  try {
+    const booking = await Booking.findById(req.params.id)
+      .populate("user")
+      .populate("hotel")
+      .populate("room")
+      .populate("discount");
+    if (!booking) {
+      res.status(404);
+      throw new Error("There is no booking by this ID");
+    }
+    res.status(200).json(booking);
+  } catch (error) {
+    next(error);
+  }
+};
