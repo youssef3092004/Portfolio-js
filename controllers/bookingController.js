a179827d7eca62c837704e1a5dce5af9b3d750d8
youssef <youssefahmedabdelkader25@gmail.com>
2025-01-01 14:43:33 +0200

    Add controller function to get all bookings
    - Implement `getBookings` to retrieve all bookings from the database, with populated fields for user, hotel, room, and discount.
    - Return the bookings as a JSON response with a 200 status code.
    - Handle the case where no bookings are found by returning a 404 error.
    - Use error handling to pass any errors to the next middleware.

---
+++ controllers/bookingController.js
@@ -0,0 +1,18 @@
+const Booking = require("../models/booking");
+
+const getBookings = async (req, res, next) => {
+  try {
+    const bookings = await Booking.find()
+      .populate("user")
+      .populate("hotel")
+      .populate("room")
+      .populate("discount");
+    if (!bookings) {
+      res.status(404);
+      throw new Error("There are no bookings available");
+    }
+    return res.status(200).json(bookings);
+  } catch (error) {
+    next(error);
+  }
+};
