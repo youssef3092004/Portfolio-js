462077ce167526c1ac2d94089ac1c77034595509
youssef <youssefahmedabdelkader25@gmail.com>
2025-01-02 21:49:24 +0200

    feat: Add scheduled jobs for updating discount statuses
    - Implemented a cron job to update discount statuses every hour using the `cron.schedule` function.
    - Added a commented-out version using `setInterval` for running the update every 5 seconds (for testing purposes).
    - Ensured that the scheduled job logs when it runs and calls the `updateDiscountStatuses` function.
    
    This update introduces a regular task to update discount statuses at fixed intervals, improving the management of discount data.

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/errorHandler");
-
+const corn = require("node-cron");
 const discountRoute = require("./routes/discountRoute");
 const bookingRoute = require("./routes/bookingRoute");
 const amenityRoute = require("./routes/amenityRoute");
-
+const { updateDiscountStatuses } = require("./controllers/discountController");
 const DB_PORT = process.env.DB_PORT || 3000;
 
 connectDB();
@@ -23,6 +23,18 @@ app.use("/api/amenities", amenityRoute);
 app.get("/", (req, res) => {
   res.send("now you in a Hello page");
 });
+
+//todo every Hour
+corn.schedule("0 * * * *", () => {
+  console.log("Running scheduled job to update discount statuses...");
+  updateDiscountStatuses();
+});
+
+//todo every 5 seconds for checking
+// setInterval(() => {
+//   console.log("Running scheduled job to update discount statuses...");
+//   updateDiscountStatuses();
+// }, 5000);
 
 app.listen(DB_PORT, () => {
   console.log(`I' listening in port ${DB_PORT}`);
