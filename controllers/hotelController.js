b883aa5a4fa853255cabe23eeb5d0eaed17c25d6
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2025-01-04 09:34:57 +0200

    docs: add documentation for getHotels function

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -1,5 +1,16 @@
 const Hotel = require("../models/hotel");
 
+/**
+ * @function getHotels
+ * @description Fetches all hotels from the database, including populated location and review details.
+ * @route GET /api/hotels
+ * @access Public
+ * @returns {Object} JSON array of hotel objects with populated location and review details.
+ * @throws {Error} If no hotels are found or if the database query fails.
+ * 
+ * This function queries the database for all hotel documents, populates associated location and review data, 
+ * and returns them as a JSON array. If no hotels are found, it responds with a 404 status.
+ */
 const getHotels = async (req, res, next) => {
   try {
     const hotels = await Hotel.find().populate("location").populate("review");
