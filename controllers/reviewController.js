4cf8063df47850231a2daadca90b657c5feb0752
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2025-01-04 11:30:51 +0200

    Add getReview function to retrieves a specific reviews from the database by its ID

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -24,6 +24,34 @@ const getReviews = async (req, res, next) => {
   }
 };
 
+/**
+ * @function getReview
+ * @description Retrieves a specific review by its ID.
+ * @route GET /api/reviews/:id
+ * @access Public
+ * @param {string} req.params.id - The ID of the review to retrieve.
+ * @returns {JSON} JSON object containing the review with user and hotel details populated.
+ * @throws {Error} If the review is not found or an invalid ID is provided.
+ *
+ * This function fetches a single review from the database using its ID
+ * and populates related user and hotel data.
+ */
+const getReview = async (req, res, next) => {
+  try {
+    const review = await Review.findById(req.params.id)
+      .populate("user")
+      .populate("hotel");
+    if (!review) {
+      res.status(404);
+      throw new Error("There is no review by this ID");
+    }
+    res.status(200).json(review);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getReviews,
+  getReview,
 };
