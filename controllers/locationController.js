75a388dd9bddd11860de5f71dd612cc1dc8946be
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2025-01-04 10:35:12 +0200

    Add getLocations function to fetch all locations from the database

---
+++ controllers/locationController.js
@@ -0,0 +1,28 @@
+const Location = require("../models/location");
+
+/**
+ * @function getLocations
+ * @description Fetches all locations from the database.
+ * @route GET /api/locations
+ * @access Public
+ * @returns {JSON} JSON array of location objects.
+ * @throws {Error} If no locations are found in the database or if the database query fails.
+ * 
+ * This function queries the database for all location documents and returns them as a JSON array. 
+ * If no locations are found, it responds with a 404 status code and an error message.
+ */
+const getLocations = async (req, res, next) => {
+  try {
+    const locations = await Location.find();
+    if (!locations) {
+      res.status(404);
+      throw new Error("There are no locations available");
+    }
+    return res.status(200).json(locations);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+module.exports = { getLocations };
