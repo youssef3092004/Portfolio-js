c38d7275cffe7f71b9cc8e93ca3c08b4a8ede17d
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2025-01-05 23:47:33 +0200

    fixbug: fix getUser function to findById req not res

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,7 +41,7 @@ const getUsers = async (req, res, next) => {
  */
 const getUser = async (req, res, next) => {
   try {
-    const user = await User.findById(res.params.id);
+    const user = await User.findById(req.params.id);
     if (!user) {
       res.status(404);
       throw new Error("There are no users available");
