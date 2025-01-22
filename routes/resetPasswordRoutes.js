484f12e73055bb9a02c7758c2fdd6b995990d27c
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2025-01-22 16:32:02 +0200

    update: add request reset password route to first request with user email to reset password

--- routes/resetPasswordRoutes.js
+++ routes/resetPasswordRoutes.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const {
+    requestReset,
     resetPassword
 } = require('../controllers/resetPasswordController');
 
 const router = express.Router();
+/**
+ * @swagger
+ * /api/auth/requestReset:
+ *   post:
+ *     summary: Request a password reset link
+ *     tags: [Authentication]
+ *     description: Allows a user to request a password reset link by providing their email address. The reset token will be sent to the provided email.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: The email address where the reset link will be sent.
+ *     responses:
+ *       200:
+ *         description: A success message with the authentication token.
+ *       400:
+ *         description: Bad request. Missing or invalid data.
+ *       401:
+ *         description: Unauthorized. Invalid email or password.
+ *       500:
+ *         description: Internal server error.
+ */
+router.post('/requestReset', requestReset);
 
 /**
  * @swagger
